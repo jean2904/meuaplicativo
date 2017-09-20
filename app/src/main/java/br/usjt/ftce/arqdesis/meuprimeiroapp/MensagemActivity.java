f0390a47c0727ecd87a58606b5d8ecc5f384bb82
arqdsis <arqdsis@mocli10217.animaedu.intranet>
2017-09-20 20:57:07 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ftce/arqdesis/meuprimeiroapp/MensagemActivity.java
@@ -0,0 +1,26 @@
+package br.usjt.ftce.arqdesis.meuprimeiroapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MensagemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensagem);
+        Intent intent = getIntent();
+        String texto = intent.getStringExtra(MainActivity.MENSAGEM);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(texto);
+
+        ViewGroup layout = (ViewGroup)findViewById(R.id.mostra_mensagem);
+        layout.addView(textView);
+
+    }
+}
